29a7ca50798491a04333b08b503c6246458ca9fc
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2016-04-12 23:47:19 +0200

    Correcting code, now it loads more than one page

--- trust-main/trustDemo.py
+++ trust-main/trustDemo.py
@@ -10,6 +10,18 @@ logger.setLevel(logging.DEBUG)
 apikey = 'tiNFg9zDKRWXaIpI8pPyWbXd4ACa9ZDt'
 
 
+def getAPIdata(_url):
+    """
+    request json from url.
+    :param _url: string - endpoint to hit.
+    :return: json - response from url.
+    """
+    req = urllib2.Request(_url)
+    req.add_header('ApiKey', apikey)
+    resp = urllib2.urlopen(req).read()
+    return json.loads(resp)
+
+
 def lambda_handler(event, context):
     """
     Event handler used for AWS Lambda service.
@@ -20,26 +32,27 @@ def lambda_handler(event, context):
         logger.debug('Domain=' + domain)
 
         sfindurl = r"https://api.trustpilot.com/v1/business-units/find?name={0}"
-        sreviewurl = r"https://api.trustpilot.com/v1/business-units/{0}/reviews?apikey={1}"
+        sreviewurl = r"https://api.trustpilot.com/v1/business-units/{0}/reviews?page={1}"
 
-        req = urllib2.Request(sfindurl.format(domain))
-        req.add_header('ApiKey', apikey)
-        resp = urllib2.urlopen(req).read()
-        response = json.loads(resp)
+        response = getAPIdata(sfindurl.format(domain))
+
         buisnessUId = response['id']
         logger.debug(buisnessUId)
 
-        reviewurl = sreviewurl.format(buisnessUId, apikey)
-        reviewstr = urllib2.urlopen(reviewurl).read()
-        d = json.loads(reviewstr)
-        logger.debug(d['reviews'])
         persons = 0
         ratingsum = 0
-        for n in d['reviews']:
-            persons += 1
-            ratingsum += int((n['stars']))
-        rating = (float(ratingsum/float(persons)))
+        # Each request returns 20 reviews per. page. We need at the most 300.
+        for x in range(1, 16):
+            reviews = getAPIdata(sreviewurl.format(buisnessUId, x))
+            if not reviews['reviews']:
+                break
+            logger.debug(reviews)
+            logger.debug(reviews['reviews'])
 
+            for review in reviews['reviews']:
+                persons += 1
+                ratingsum += int((review['stars']))
+            rating = (float(ratingsum/float(persons)))
         return {"Result": "Successfully", "Domain": domain, "RatingReslut": {"Ratings": persons, "Average": rating}}
     except Exception as e:
         logger.error("!!!! -- CRITICAL ERROR -- !!!! := " + str(e.message))
@@ -47,7 +60,6 @@ def lambda_handler(event, context):
 
 
 if __name__ == '__main__':
-    _event = {"Domain": "www.dan-ejendomme.dk"}
+    _event = {"Domain": "www.bookings.com"}
     test = lambda_handler(_event, None)
-
     print (test)
